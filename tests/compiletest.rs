c6ae2855d2ad9c4b0f7e4e646ca07cf25aa638db
Manish Goregaokar <manishsmail@gmail.com>
2015-05-09 15:28:38 +0530

    fix

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -1,15 +1,15 @@
-extern crate compiletest;
+extern crate compiletest_rs;
 
 use std::path::PathBuf;
 
 fn run_mode(mode: &'static str) {
-    let mut config = compiletest::default_config();
+    let mut config = compiletest_rs::default_config();
     let cfg_mode = mode.parse().ok().expect("Invalid mode");
 
     config.mode = cfg_mode;
     config.src_base = PathBuf::from(format!("tests/{}", mode));
 
-    compiletest::run_tests(&config);
+    compiletest_rs::run_tests(&config);
 }
 
 #[test]
